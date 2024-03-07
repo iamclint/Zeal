56688447e8ba59ea7dcaf3a7c0c0d8f0660c5f3e
Samantha Bartlett <sammynilla@protonmail.com>
2024-03-06 19:19:49 -0500

    various updates

--- Zeal/auto_stand.h
+++ Zeal/auto_stand.h
@@ -6,7 +6,11 @@ class AutoStand
 public:
 	void handle_movement_binds(int cmd, bool key_down);
 	void handle_spellcast_binds(int cmd);
-	AutoStand(class ZealService* zeal);
+	AutoStand(class ZealService* zeal, class IO_ini* ini);
 	~AutoStand();
+private:
+	void load_settings(IO_ini* ini);
+	bool spellbook_left_autostand;
+	bool spellbook_right_autostand;
 };
 
