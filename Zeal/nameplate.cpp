e32d9448ad0aa9e9228e3ed9e9b4cbf57f4b3b04
adonislive <156614476+adonislive@users.noreply.github.com>
2024-10-04 00:06:55 -0700

    Added NameplateColors to Zeal
    Feature adds better visibility to Nameplate Colors for Raids, Guilds, Groups, AFK, and LD situations. A player on Discord had requested I submit what I had done so far.
    
    Two ways to use feature currently:
    1. /nameplatecolors command to toggle feature
    2. bind key available to toggle feature
    
    Feature adds itself the the eqclient.ini and remembers the bool setting that player sets for next gaming session.
    
    When feature is off the client has normal hardcoded Nameplate Colors.
    
    When feature is on the client has the following Nameplate Colors for better visibility in game:
    
    Raid Members:  Black
    Guild Members: Light Green
    Group Members: Greenish Blue
    Away from Keyboard-AFK:  Orange
    Linkdead-LD:  Red
    
    Open to future enhancements, mods, and feedback.

--- Zeal/nameplate.cpp
+++ Zeal/nameplate.cpp
@@ -3,6 +3,8 @@
 #include "EqAddresses.h"
 #include "string_util.h"
 
+bool nameplatecolorsEnabled = true;
+
 struct CachedNameData
 {
 	int unk1;
@@ -37,11 +39,90 @@ int DeferCachedNameTagTextW(int font_size, CachedNameData* cnd, int a1, int a2, 
 	//}
 }
 
+int __fastcall SetNameSpriteTint(void* this_ptr, void* not_used, Zeal::EqStructures::Entity* spawn)
+{	
+	int result = ZealService::get_instance()->hooks->hook_map["SetNameSpriteTint"]->original(SetNameSpriteTint)(this_ptr, not_used, spawn);
+	if (spawn == NULL) { return result; }
+	if (spawn->ActorInfo->DagHeadPoint == NULL) { return result; }
+	if (spawn->ActorInfo->DagHeadPoint->StringSprite == NULL) { return result; }
+	if (nameplatecolorsEnabled) {
+		int maxGroupMembers = 5;
+		int maxRaidMembers = 72;
+		Zeal::EqStructures::Entity** groupmembers = reinterpret_cast<Zeal::EqStructures::Entity**>(Zeal::EqGame::GroupList);
+		Zeal::EqStructures::RaidMember* raidMembers = reinterpret_cast<Zeal::EqStructures::RaidMember*>(Zeal::EqGame::RaidMemberList);
+		switch (spawn->Type) {
+		case 0: //Players
+			if (spawn == Zeal::EqGame::get_target()) //Leave blinking indicator on target
+				return result;
+			if (raidMembers){
+				for (int i = 0; i < maxRaidMembers; ++i) //Raid Member loop
+				{
+					Zeal::EqStructures::RaidMember member = raidMembers[i];
+					if ((member.GroupNumber == 0xFFFFFFFE) || (strlen(member.Name) == 0) || (strcmp(member.Name, Zeal::EqGame::get_self()->Name) == 0))
+						continue;
+					Zeal::EqStructures::Entity* raidMember = ZealService::get_instance()->entity_manager->Get(member.Name);
+					if (!raidMember)
+						continue;
+					if (spawn == raidMember)
+						raidMember->ActorInfo->DagHeadPoint->StringSprite->Color = 0xFF000000; //Raid Member-Black
+				}
+			}
+			if (spawn->GuildId == Zeal::EqGame::get_self()->GuildId) //Guild Member
+				spawn->ActorInfo->DagHeadPoint->StringSprite->Color = 0xFF00FF80; //Guild Member-Greenish Blue
+				//spawn->ActorInfo->DagHeadPoint->StringSprite->Color = 0xFF80FF80; //Guild Member-White Green
+			if (groupmembers) {
+				for (int i = 0; i < maxGroupMembers; ++i) //Group Member loop
+				{
+					Zeal::EqStructures::Entity* groupmember = groupmembers[i];
+					if (spawn == groupmember)
+						spawn->ActorInfo->DagHeadPoint->StringSprite->Color = 0x0500FF32; //Group Member-Light Green 
+					continue;
+				}
+			}
+			if (spawn->IsLinkDead == 1) //LinkDead
+				spawn->ActorInfo->DagHeadPoint->StringSprite->Color = 0xFFFF0000; //LinkDead - Red
+			else if (spawn->IsAwayFromKeyboard == 1) //AFK
+				spawn->ActorInfo->DagHeadPoint->StringSprite->Color = 0xFFFF8000; //AFK - Orange	
+			break;
+		case 1: //NPC
+			break;
+		case 2: //NPC Corpse
+			break;
+		case 3: //Player Corpse
+			break;
+		default:
+			break;
+		}
+	}
+	return result;
+}
+
+void NamePlate::set_enabled(bool _enabled)
+{
+	_enabled = !nameplatecolorsEnabled;
+	ZealService::get_instance()->ini->setValue<bool>("Zeal", "NameplateColors", _enabled);
+	nameplatecolorsEnabled = !nameplatecolorsEnabled;
+	Zeal::EqGame::print_chat("NameplateColors are %s", _enabled ? "Enabled" : "Disabled");
+}
+
 NamePlate::NamePlate(ZealService* zeal, IO_ini* ini)
 {
 	HMODULE eqfx = GetModuleHandleA("eqgfx_dx8.dll");
 	if (eqfx)
 		zeal->hooks->Add("DeferCachedNameTagTextW", (DWORD)eqfx + 0x70A00, DeferCachedNameTagTextW, hook_type_detour);
+	if (nameplatecolorsEnabled)
+		zeal->hooks->Add("SetNameSpriteTint", 0x4B114D, SetNameSpriteTint, hook_type_detour);
+	
+	if (!ini->exists("Zeal", "NameplateColors")) 
+		ini->setValue<bool>("Zeal", "NameplateColors", false);
+	if (ini->exists("Zeal", "NameplateColors"))
+		nameplatecolorsEnabled = ini->getValue<bool>("Zeal", "NameplateColors");
+
+	zeal->commands_hook->Add("/nameplatecolors", {}, "Toggles Nameplate Colors",
+		[this](std::vector<std::string>& args) {
+			set_enabled(!ZealService::get_instance()->nameplate->is_enabled());
+			return true;
+		});
 }
 
 NamePlate::~NamePlate()
--- Zeal/nameplate.h
+++ Zeal/nameplate.h
@@ -3,9 +3,13 @@
 #include "memory.h"
 #include <stdint.h>
 #include "EqStructures.h"
+
 class NamePlate
 {
 public:
+	bool nameplateColors = false;
+	bool is_enabled() const { return nameplateColors; }
+	void set_enabled(bool enabled);	
 	NamePlate(class ZealService* zeal, class IO_ini* ini);
 	~NamePlate();
 private:
