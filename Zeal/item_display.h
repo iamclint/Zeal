be102fe57b507dd139b7a52d54a116c96cba3c5c
Crass <35114856+crass-public@users.noreply.github.com>
2024-10-06 00:20:40 -0400

    Save current item from SetItem instead of trying to grab ESI

--- Zeal/item_display.h
+++ Zeal/item_display.h
@@ -9,6 +9,7 @@ public:
 	ItemDisplay(class ZealService* pHookWrapper, class IO_ini* ini);
 	~ItemDisplay();
 	Zeal::EqUI::ItemDisplayWnd* get_available_window(Zeal::EqStructures::_EQITEMINFO* item);
+	Zeal::EqStructures::_EQITEMINFO* current_item;
 	std::vector<Zeal::EqUI::ItemDisplayWnd*> windows;
 private:
 	void init_ui();
