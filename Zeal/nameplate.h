b4ecb52f3ea849f767fe63d5c555e65e3795204c
adonislive <156614476+adonislive@users.noreply.github.com>
2024-11-06 07:32:42 -0800

    Added Show Nameplate Options at Character Select to nameplate.h

--- Zeal/nameplate.h
+++ Zeal/nameplate.h
@@ -12,17 +12,20 @@ public:
 	bool nameplateSelf = false;
 	bool nameplateX = false;
 	bool nameplateRaidPets = false;
+	bool nameplateCharSelect = false;
 
 	bool colors_is_enabled() const { return nameplateColors; }
 	bool con_colors_is_enabled() const { return nameplateconColors; }
 	bool self_is_enabled() const { return nameplateSelf; }
 	bool x_is_enabled() const { return nameplateX; }
 	bool raidpets_is_enabled() const { return nameplateRaidPets; }
+	bool charselect_is_enabled() const { return nameplateCharSelect; }
 	void colors_set_enabled(bool enabled);	
 	void con_colors_set_enabled(bool enabled);
 	void self_set_enabled(bool enabled);
 	void x_set_enabled(bool enabled);
 	void raidpets_set_enabled(bool enabled);
+	void charselect_set_enabled(bool enabled);
 	void HandleState(void* this_ptr, void* not_used, Zeal::EqStructures::Entity* spawn);
 	void HandleTint(Zeal::EqStructures::Entity* spawn);
 	NamePlate(class ZealService* zeal, class IO_ini* ini);
