5fb8b254258afdbb54982b005c66e0bf99ee6a7b
adonislive <156614476+adonislive@users.noreply.github.com>
2024-11-06 10:37:51 -0800

    Add Target Color,Marker,Health to nameplate.h

--- Zeal/nameplate.h
+++ Zeal/nameplate.h
@@ -13,6 +13,9 @@ public:
 	bool nameplateX = false;
 	bool nameplateRaidPets = false;
 	bool nameplateCharSelect = false;
+	bool nameplateTargetColor = false;
+	bool nameplateTargetMarker = false;
+	bool nameplateTargetHealth = false;
 
 	bool colors_is_enabled() const { return nameplateColors; }
 	bool con_colors_is_enabled() const { return nameplateconColors; }
@@ -20,12 +23,18 @@ public:
 	bool x_is_enabled() const { return nameplateX; }
 	bool raidpets_is_enabled() const { return nameplateRaidPets; }
 	bool charselect_is_enabled() const { return nameplateCharSelect; }
+	bool target_color_is_enabled() const { return nameplateTargetColor; }
+	bool target_marker_is_enabled() const { return nameplateTargetMarker; }
+	bool target_health_is_enabled() const { return nameplateTargetHealth; }
 	void colors_set_enabled(bool enabled);	
 	void con_colors_set_enabled(bool enabled);
 	void self_set_enabled(bool enabled);
 	void x_set_enabled(bool enabled);
 	void raidpets_set_enabled(bool enabled);
 	void charselect_set_enabled(bool enabled);
+	void target_color_set_enabled(bool enabled);
+	void target_marker_set_enabled(bool enabled);
+	void target_health_set_enabled(bool enabled);
 	void HandleState(void* this_ptr, void* not_used, Zeal::EqStructures::Entity* spawn);
 	void HandleTint(Zeal::EqStructures::Entity* spawn);
 	NamePlate(class ZealService* zeal, class IO_ini* ini);
