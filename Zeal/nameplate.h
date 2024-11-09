45e00e3861ae59ab60d19d5d124d4aeeae0691df
adonislive <156614476+adonislive@users.noreply.github.com>
2024-11-08 19:23:43 -0800

    Update nameplate.h - add /nameplatehideself and /nameplatehideraidpets

--- Zeal/nameplate.h
+++ Zeal/nameplate.h
@@ -9,9 +9,9 @@ class NamePlate
 public:
 	bool nameplateColors = false;
 	bool nameplateconColors = false;
-	bool nameplateSelf = false;
+	bool nameplateHideSelf = false;
 	bool nameplateX = false;
-	bool nameplateRaidPets = false;
+	bool nameplateHideRaidPets = false;
 	bool nameplateCharSelect = false;
 	bool nameplateTargetColor = false;
 	bool nameplateTargetMarker = false;
@@ -19,18 +19,18 @@ public:
 
 	bool colors_is_enabled() const { return nameplateColors; }
 	bool con_colors_is_enabled() const { return nameplateconColors; }
-	bool self_is_enabled() const { return nameplateSelf; }
+	bool hide_self_is_enabled() const { return nameplateHideSelf; }
 	bool x_is_enabled() const { return nameplateX; }
-	bool raidpets_is_enabled() const { return nameplateRaidPets; }
+	bool hide_raidpets_is_enabled() const { return nameplateHideRaidPets; }
 	bool charselect_is_enabled() const { return nameplateCharSelect; }
 	bool target_color_is_enabled() const { return nameplateTargetColor; }
 	bool target_marker_is_enabled() const { return nameplateTargetMarker; }
 	bool target_health_is_enabled() const { return nameplateTargetHealth; }
 	void colors_set_enabled(bool enabled);	
 	void con_colors_set_enabled(bool enabled);
-	void self_set_enabled(bool enabled);
+	void hide_self_set_enabled(bool enabled);
 	void x_set_enabled(bool enabled);
-	void raidpets_set_enabled(bool enabled);
+	void hide_raidpets_set_enabled(bool enabled);
 	void charselect_set_enabled(bool enabled);
 	void target_color_set_enabled(bool enabled);
 	void target_marker_set_enabled(bool enabled);
