925c9fb09d66d7905f1323060eb1085d38f676e1
CoastalRedwood <172213097+CoastalRedwood@users.noreply.github.com>
2024-11-14 20:46:40 -0800

    Switch to using client's EQP_IDArray as ID LUT
    - Switched the get_entity_by_id() to use the IDArray instead
      of performing a traversal of the entity list
    - Switched EntityManager's Get by ID to use get_entity_by_id
    - Added a sanity check to the EntityManaget.Get(name) to ensure
      the entity matches the client's IDArray

--- Zeal/EntityManager.cpp
+++ Zeal/EntityManager.cpp
@@ -5,15 +5,18 @@
 
 void EntityManager::Add(struct Zeal::EqStructures::Entity* ent)
 {
-	entity_map[ent->Name] = ent;
-	entity_id_map[ent->SpawnId] = ent;
-
-	Zeal::EqStructures::Entity* self = Get(0);
-	if (self && self->SpawnId != 0)
-	{
-		entity_id_map[self->SpawnId] = self;
-		entity_id_map.erase(0);
-	}
+	// Perform simple validity checks before adding an entity.
+	// Notes:
+	// - The client StartNetworkGame method contructs a placeholder for _LocalPlayer
+	// with EQPlayer::EQPlayer(nullptr,0,1,1,"load") and a zero id that is later deconstructed
+	// - Entering a zone constructs a new Self EQPlayer that starts with a zero spawn_id.
+	// - The server likes to reuse npc names (a_bat039) shortly after they die even while the
+	// previous corpse is still there. This will overwrite the map with the new entity, so
+	// the entity manager map's count will be reduced relative to the IDArray count.
+	if (!ent)
+		return;
+	if (ent->SpawnId == 0 || ent == Zeal::EqGame::get_entity_by_id(ent->SpawnId))
+		entity_map[ent->Name] = ent;
 }
 
 void EntityManager::Remove(struct Zeal::EqStructures::Entity* ent)
@@ -24,43 +27,71 @@ void EntityManager::Remove(struct Zeal::EqStructures::Entity* ent)
 	for (auto it = entity_map.begin(); it != entity_map.end(); ++it)
 	{
 		if (it->second == ent) {
-			entity_id_map.erase(it->second->SpawnId);
 			entity_map.erase(it);  // Ignoring updated iterator.
 			return;
 		}
 	}
 }
 
-Zeal::EqStructures::Entity* EntityManager::Get(std::string  name) const {
+Zeal::EqStructures::Entity* EntityManager::Get(std::string name) const {
 	if (!Zeal::EqGame::is_in_game())
 		return nullptr;
 	auto it = entity_map.find(name);
-	return (it == entity_map.end()) ? nullptr : it->second;
+	if (it != entity_map.end()) {
+		// Only return a hit if the manager is in sync with the client EQP_IDArray.
+		auto* entity = it->second;
+		if (entity && entity == Zeal::EqGame::get_entity_by_id(entity->SpawnId))
+			return entity;
+	}
+	return nullptr;
 }
+
 Zeal::EqStructures::Entity* EntityManager::Get(WORD id) const
 {
-	if (!Zeal::EqGame::is_in_game())
-		return nullptr;
-	auto it = entity_id_map.find(id);
-	return (it == entity_id_map.end()) ? nullptr : it->second;
+	auto* entity = Zeal::EqGame::get_entity_by_id(id);
+	return entity;
 }
 
 // Local Dump() helper that confirms there isn't a mismatch of a hashmap key/value pair.
 static bool is_valid_entity(const std::string& name, struct Zeal::EqStructures::Entity* entity) {
-	Zeal::EqStructures::Entity* current = Zeal::EqGame::get_entity_list();
+	if (!entity)
+		return false;
+	auto* client_entity = Zeal::EqGame::get_entity_by_id(entity->SpawnId);  // Returns nullptr if fails.
+	if (client_entity != entity) {
+		Zeal::EqGame::print_chat("MISMATCH: client[%i] entity does not match for name: %s", entity->SpawnId, name.c_str());
+		return false;
+	} else if (name != client_entity->Name) {
+		if (strcmp(Zeal::EqGame::trim_name(name.c_str()), Zeal::EqGame::trim_name(client_entity->Name)) == 0)
+			Zeal::EqGame::print_chat("NAME_CHANGED[%i]: manager: %s, client: %s", entity->SpawnId, name.c_str(), client_entity->Name);
+		else
+			Zeal::EqGame::print_chat("BADNAME[%i]: manager: %s, client: %s", entity->SpawnId, name.c_str(), client_entity->Name);
+		return false;
+	}
+	return true;
+}
 
+// Local dump helper that reports any missing entries in the table.
+static int report_missing_entities() {
+	Zeal::EqStructures::Entity* current = Zeal::EqGame::get_entity_list();
+	auto entity_manager = ZealService::get_instance()->entity_manager.get();  // Short-term ptr.
+
+	int missing_corpse_count = 0;
+	int missing_count = 0;
 	while (current != nullptr) {
-		if (current == entity) {
-			if (name != current->Name) {
-				Zeal::EqGame::print_chat("MISMATCH: entity name: %s, manager name: %s", current->Name, name.c_str());
-				return false;
+		if (!entity_manager->Get(current->Name)) {
+			if (current->Type == Zeal::EqEnums::NPCCorpse)
+				++missing_corpse_count;
+			else {
+				++missing_count;
+				Zeal::EqGame::print_chat("MISSING: entity[%i]: %s", current->SpawnId, current->Name);
 			}
-			return true;
 		}
 		current = current->Next;
 	}
+	if (missing_corpse_count)
+		Zeal::EqGame::print_chat("Missing corpse count: %i", missing_corpse_count);
 
-	return false;
+	return missing_count;
 }
 
 // Local Dump() helper that just counts up the number of EQ game entities.
@@ -76,18 +107,18 @@ static int get_num_game_entities() {
 
 void EntityManager::Dump() const {
 
-	Zeal::EqGame::print_chat("Num EQ entities: %i, Num in manager: %i, Num in spawn_id: %i",
-		get_num_game_entities(), entity_map.size(), entity_id_map.size());
+	Zeal::EqGame::print_chat("Num EQ entities: %i, Num in manager: %i",
+			get_num_game_entities(), entity_map.size());
 
 	for (const auto& [key, value] : entity_map)
 	{
 		// First do an integrity check to make sure the entity address is valid.
-		if (!is_valid_entity(key, value))
-			Zeal::EqGame::print_chat("INVALID: name: %s, entity: 0x%08x",
-				key.c_str(), reinterpret_cast<uint32_t>(value));
-		else if (value->Type == Zeal::EqEnums::EntityTypes::Player)
-			Zeal::EqGame::print_chat("name: %s, entity: 0x%08x", key.c_str(), reinterpret_cast<uint32_t>(value));
+		if (is_valid_entity(key, value) && (value->Type == Zeal::EqEnums::EntityTypes::Player))
+			Zeal::EqGame::print_chat("[%i]: name: %s, entity: 0x%08x", value->SpawnId,
+									key.c_str(), reinterpret_cast<uint32_t>(value));
 	}
+
+	report_missing_entities();
 }
 
 EntityManager::EntityManager(class ZealService* zeal, class IO_ini* ini)
