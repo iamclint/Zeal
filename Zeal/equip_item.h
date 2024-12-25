bbf2ffa35c2105bc33a19c4cb10ff268dacb3d97
Cord B <295783+cord-b@users.noreply.github.com>
2024-12-25 01:20:27 -0800

    Added UI Option to toggle RightClickToEquip

--- Zeal/equip_item.h
+++ Zeal/equip_item.h
@@ -9,5 +9,16 @@ class EquipItem
 public:
 	EquipItem(class ZealService* pHookWrapper, class IO_ini* ini);
 	~EquipItem();
+	void HandleRButtonUp(Zeal::EqUI::InvSlotWnd* wnd, int mouse_x, int mouse_y);
+	void SetEnabled(bool in_enabled);
+	bool enabled;
+private:
+	bool ClickInventoryWindowSlot(int invSlot, unsigned int flags = 0);
+	int EQUIP_PRIORITY_ORDER[21] = {
+		12, 13, 10,           // Primary, Secondary, Range
+		16, 17, 1, 6, 11, 18, // Chest, Legs, Head, Arms, Hands, Feet
+		5, 7, 4, 2, 19, 8, 9, // Shoulders, Back, Neck, Face, Waist, WristLeft, WristRight,
+		0, 3, 14, 15, 20      // EarLeft, EarRight, RingLeft, RingRight, Ammo
+	};
 };
 
