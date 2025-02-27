80db155c57a586bbf1cfff9188a9ec56412d483c
SherraBard <172213097+CoastalRedwood@users.noreply.github.com>
2025-02-27 14:25:34 -0800

    Bump to 0.6.5 (#294)

--- Zeal/Zeal.h
+++ Zeal/Zeal.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "framework.h"
-#define ZEAL_VERSION "0.6.5-beta3"
+#define ZEAL_VERSION "0.6.5"
 #ifndef ZEAL_BUILD_VERSION  // Set by github actions
 #define ZEAL_BUILD_VERSION "UNOFFICIAL"  // Local build
 #endif
